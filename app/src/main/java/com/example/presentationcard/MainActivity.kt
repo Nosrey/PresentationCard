446e3093be8003932ad85b8c0c0eadcb701f5e13
Nosrey <nosrey135@gmail.com>
2024-05-14 23:47:13 -0400

    design advanced, now the app is finished

--- app/src/main/java/com/example/presentationcard/MainActivity.kt
+++ app/src/main/java/com/example/presentationcard/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -19,8 +22,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.presentationcard.ui.theme.PresentationCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +36,7 @@ class MainActivity : ComponentActivity() {
             PresentationCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     Presentation()
                 }
@@ -43,68 +48,101 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Presentation(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier,
+        // pongo de fondo el color #f2f5f3
+        modifier = modifier.background(Color(0xFFF2F5F3)),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.SpaceBetween,
     ) {
+        Spacer(Modifier.weight(0.5f))
         PresentationHeader()
+        Spacer(Modifier.weight(0.5f))
         PresentationFooter()
     }
 }
 
 @Composable
 fun PresentationHeader(modifier: Modifier = Modifier) {
-    Text(
-        text = "Yerson Rico",
-        modifier = modifier,
-        textAlign = TextAlign.Center,
-    )
-    val image = painterResource(id = R.drawable.android_icon)
-    Image(
-        painter = image,
-        contentDescription = "Android Icon",
-        // width del 30% del ancho de la pantalla
-        modifier = Modifier.fillMaxWidth(0.3f)
-    )
-    Text(
-        text = "Android Developer",
-        modifier = modifier
-    )
+    Column(modifier = modifier.padding(top = 32.dp)) {
+        val image = painterResource(id = R.drawable.android_icon)
+        Image(
+            painter = image, contentDescription = "Android Icon",
+            // width del 30% del ancho de la pantalla
+            modifier = Modifier
+                .fillMaxWidth(0.3f)
+                .padding(top = 2.dp, bottom = 2.dp, start = 0.dp, end = 0.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = "Yerson Rico",
+            modifier = modifier.align(Alignment.CenterHorizontally),
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Android Developer",
+            modifier = modifier.align(Alignment.CenterHorizontally),
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            // el color del texto debe ser: #071f00
+            color = Color(0xFF071F00)
+        )
+    }
 }
 
 @Composable
 fun PresentationFooter(modifier: Modifier = Modifier) {
-    Column {
+    Column(modifier = modifier.padding(bottom = 32.dp).fillMaxWidth(0.6f)) {
         val phoneIcon = painterResource(R.drawable.phone_icon)
         val emailIcon = painterResource(R.drawable.email_icon)
         val socialIcon = painterResource(R.drawable.social_icon)
 
-        Row {
+        Row(modifier = modifier.fillMaxWidth().padding(bottom = 8.dp), horizontalArrangement = Arrangement.Start) {
             Icon(
                 painter = phoneIcon,
                 contentDescription = "Phone Icon",
                 tint = Color.Black,
-                modifier = Modifier.fillMaxWidth(0.07f)
+                modifier = Modifier
+                    .width(32.dp)
+                    .align(Alignment.CenterVertically)
+                    .padding(end = 12.dp)
             )
-            Text(text = "+58 412 541 0165")
+            Text(
+                text = "+58 412 541 0165",
+                modifier = Modifier.align(Alignment.CenterVertically),
+                fontSize = 18.sp
+            )
         }
-        Row {
+        Row(modifier = modifier.fillMaxWidth().padding(bottom = 8.dp), horizontalArrangement = Arrangement.Start) {
             Icon(
                 painter = emailIcon,
                 contentDescription = "Email Icon",
                 tint = Color.Black,
-                modifier = Modifier.fillMaxWidth(0.06f)
+                modifier = Modifier
+                    .width(32.dp)
+                    .align(Alignment.CenterVertically)
+                    .padding(end = 12.dp)
             )
-            Text(text = "nosrey135@gmail.com")
+            Text(
+                text = "nosrey135@gmail.com",
+                modifier = Modifier.align(Alignment.CenterVertically),
+                fontSize = 18.sp
+            )
         }
-        Row {
+        Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
             Icon(
                 painter = socialIcon,
                 contentDescription = "Social Icon",
                 tint = Color.Black,
-                modifier = Modifier.fillMaxWidth(0.07f)
+                modifier = Modifier
+                    .width(32.dp)
+                    .align(Alignment.CenterVertically)
+                    .padding(end = 12.dp)
             )
-            Text(text = "@YersonRicoDev")
+            Text(
+                text = "@YersonRicoDev",
+                modifier = Modifier.align(Alignment.CenterVertically),
+                fontSize = 18.sp
+            )
         }
 
     }
